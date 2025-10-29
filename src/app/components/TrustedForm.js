9bbb04d317075844df334577701371b01722cb16
Nikola Stanin <nikolastanin@gmail.com>
2025-10-29 23:46:33 +0100

    bla

--- src/app/components/TrustedForm.js
+++ src/app/components/TrustedForm.js
@@ -1,38 +1,31 @@
 "use client";
-import { useEffect } from "react";
 
 export default function TrustedForm() {
-    useEffect(() => {
-        try {
-            const existing = document.getElementById("trustedform-script");
-            if (existing) return;
-
-            const tf = document.createElement("script");
-            tf.type = "text/javascript";
-            tf.async = true;
-            tf.id = "trustedform-script";
-            tf.src = ("https:" === document.location.protocol ? "https" : "http") +
-                "://api.trustedform.com/trustedform.js?field=xxTrustedFormCertUrl&use_tagged_consent=true&l=" +
-                (new Date().getTime() + Math.random());
-
-            const firstScript = document.getElementsByTagName("script")[0];
-            if (firstScript && firstScript.parentNode) {
-                firstScript.parentNode.insertBefore(tf, firstScript);
-            } else {
-                document.body.appendChild(tf);
-            }
-        } catch (_) {
-            // No-op if DOM not available
-        }
-    }, []);
-
     return (
-        <div style={{ display: "none" }} aria-hidden="true">
-            <input type="hidden" name="xxTrustedFormCertUrl" value="" />
-            <input type="hidden" name="xxTrustedFormPingUrl" value="" />
+        <>
+            <div style={{ display: "none" }} aria-hidden="true">
+                <input type="hidden" name="xxTrustedFormCertUrl" value="" />
+                <input type="hidden" name="xxTrustedFormPingUrl" value="" />
+            </div>
+            <script
+                type="text/javascript"
+                dangerouslySetInnerHTML={{
+                    __html: `
+  (function() {
+    var tf = document.createElement('script');
+    tf.type = 'text/javascript';
+    tf.async = true;
+    tf.src = ("https:" == document.location.protocol ? 'https' : 'http') +
+      '://api.trustedform.com/trustedform.js?field=xxTrustedFormCertUrl&use_tagged_consent=true&l=' +
+      (new Date().getTime() + Math.random());
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(tf, s);
+  })();
+                `.trim()
+                }}
+            />
             <noscript>
                 <img src="https://api.trustedform.com/ns.gif" alt="" />
             </noscript>
-        </div>
+        </>
     );
 }
